983c908c3b291a1bf31b2be351d2047dafb39507
LaylaTD <layla.tamb@gmail.com>
2024-06-06 15:29:59 -0400

    correction bug width

--- views/utilisateurs/create.view.php
+++ views/utilisateurs/create.view.php
@@ -3,32 +3,31 @@
 <main>
 
     <!-- Début Message Utilisateur -->
-    <?php if(isset($_GET["informations_requises"])) : ?>
-        <section class="message-utilisateur">
-            <p>Tu as oublié des éléments essentiels pour ton aventure, complète <span>tous les champs</span> avant de continuer!</p>
-        </section>
-    <?php endif; ?>
+            <?php if(isset($_GET["informations_requises"])) : ?>
+                <section class="message-utilisateur">
+                    <p>Tu as oublié des éléments essentiels pour ton aventure, complète <span>tous les champs</span> avant de continuer!</p>
+                </section>
+            <?php endif; ?>
 
-    <?php if(isset($_GET["mdp_invalide"])) : ?>
-        <section class="message-utilisateur">
-            <p>Tes mots de passe ne correspondent pas. Assure-toi qu'ils sont <span>identiques</span> pour déverrouiller la suite!</p>
-        </section>
-    <?php endif; ?>
+            <?php if(isset($_GET["mdp_invalide"])) : ?>
+                <section class="message-utilisateur">
+                    <p>Tes mots de passe ne correspondent pas. Assure-toi qu'ils sont <span>identiques</span> pour déverrouiller la suite!</p>
+                </section>
+            <?php endif; ?>
 
-    <?php if(isset($_GET["erreur_courriel"])) : ?>
-        <section class="message-utilisateur">
-            <p>L'adresse e-mail existe déjà. Veuillez vous 
-                <a href="index">connecter</a>
-            </p>
-        </section>
-    <?php endif; ?>
+            <?php if(isset($_GET["erreur_courriel"])) : ?>
+                <section class="message-utilisateur">
+                    <p>
+                        Hé, cette adresse e-mail est déjà utilisée! <a href="index">Connecte-toi</a> pour continuer ton aventure.
+                    </p>
+                </section>
+            <?php endif; ?>
     <!-- Fin Message Utilisateur -->
 
     <section class="formulaire">
         <h1>Création de compte</h1>
     
         <form action="compte-enregistrer" method="POST">
-    
             <input type="text" name="prenom" placeholder="Prénom" autofocus>
             <input type="text" name="nom" placeholder="Nom">
             <input type="email" name="email" placeholder="Adresse e-mail">
