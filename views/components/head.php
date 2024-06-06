19139341759bbfdec1fe9fc3aad01ac3de64da2f
LaylaTD <layla.tamb@gmail.com>
2024-06-06 14:23:46 -0400

    Design accueil

--- views/components/head.php
+++ views/components/head.php
@@ -7,7 +7,7 @@
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Jersey+15&family=Rubik:ital,wght@0,300..900;1,300..900&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:ital,wght@0,200..800;1,200..800&family=Tiny5&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="public/css/style.css">
 </head>
 <body>
--- views/index.view.php
+++ views/index.view.php
@@ -3,18 +3,12 @@
 <main>
 
     <section class="authentification">
+        <h1>Le Guide du Mineur</h1>
+
         <form action="compte-connecter" method="POST">
-            <label>
-                Adresse e-mail
-                <input type="email" name="email" placeholder="Adresse e-mail" autofocus>
-            </label>
-
-            <label>
-                Mot de passe
-                <input type="password" name="mot_de_passe" placeholder="Mot de passe">
-            </label>
-
-            <input type="submit" value="Se connecter">
+            <input type="email" name="email" placeholder="Adresse e-mail" autofocus>
+            <input type="password" name="mot_de_passe" placeholder="Mot de passe">
+            <input type="submit" value="Se connecter" class="btn-submit">
         </form>
 
         <a href="compte-creer">Créer votre compte</a>
