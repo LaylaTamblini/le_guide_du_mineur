25caa3365b67dfc361635c4cfc06dcc68db3b874
LaylaTamblini <layla.tamb@gmail.com>
2024-06-08 16:51:40 -0400

    Message erreur activité

--- views/activites/create.view.php
+++ views/activites/create.view.php
@@ -3,23 +3,43 @@
 <a href="activites">Retour</a>
 <h1>Création d'une activité</h1>
 
+    <!-- Début Message Utilisateur -->
+    <?php if(isset($_GET["informations_requises"])) : ?>
+        <section class="message-utilisateur" v-if="messageUtilisateur">
+            <span class="material-icons">
+                priority_high
+            </span>
+            
+            <p>Aventurier, tu dois compléter <span>tous les champs</span>, sauf l'image, avant de continuer.</p>
+            
+            <button @click.prevent="fermerMessage()">
+                <span class="material-icons">
+                    close
+                </span>
+            </button>
+        </section>
+    <?php endif; ?>
+
+    <?php if(isset($_GET["echec_activite"])) : ?>
+        <section class="message-utilisateur" v-if="messageUtilisateur">
+            <span class="material-icons">
+                priority_high
+            </span>
+            
+            <p>Quelque chose s'est mal passé lors de la création du compte. <span>Réessaie plus tard.</span></p>
+            
+            <button @click.prevent="fermerMessage()">
+                <span class="material-icons">
+                    close
+                </span>
+            </button>
+        </section>
+    <?php endif; ?>
+    <!-- Fin Message Utilisateur -->
+
 <section class="formulaire">
     <form action="activites-enregistrer" method="POST" enctype="multipart/form-data">
-        
-        <!-- Début Message Utilisateur -->
-        <?php if(isset($_GET["informations_requises"])) : ?>
-            <section class="message-utilisateur">
-                <p>Tous les champs sont requis sauf l'image. Merci de les remplir pour continuer.</p>
-            </section>
-        <?php endif; ?>
 
-        <?php if(isset($_GET["echec_activite"])) : ?>
-            <section class="message-utilisateur">
-                <p>L'activité n'a pas été ajouté. Veuillez réessayez plus tard.</p>
-            </section>
-        <?php endif; ?>
-        <!-- Fin Message Utilisateur -->
-        
         <input type="text" name="titre" placeholder="Titre de l'activité" autofocus>
         <input type="file" name="image" placeholder="Image de l'activité">
 
